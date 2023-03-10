426be67c4045f85f65a70db3fee01d0d6160b22d
MarkusKha <111678949+MarkusKha@users.noreply.github.com>
2023-03-10 16:33:47 +1100

    Physics 2D registered

--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
 import gsap from 'gsap'
+gsap.registerPlugin(Physics2DPlugin)
 gsap.to('.app-links', { xPercent: 100, duration: 1 })
--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     hmr: {
       host: 'localhost',
       protocol: 'ws',
+      overlay: false,
     },
   },
   build: {
